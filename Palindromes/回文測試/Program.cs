400531a2669ea1447e22dda7500957b5b53273ee
王鈞則 <ex29872678@gmail.com>
2018-05-15 20:50:28 +0800

    Init

---
+++ Palindromes/回文測試/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace 回文測試
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("輸入一串字串");
+            var x = Console.ReadLine();
+            ReverseArray array = new ReverseArray();
+            if(array.Rever(x))
+            {
+                Console.WriteLine("這是回文");
+            }
+            else
+            {
+                Console.WriteLine("這不是回文");
+            }
+        }
+    }
+}
