d42d0a1bc2a44b9192bcfdbaaa8a85060a3242c5
CHUNTSE <ex29872678@gmail.com>
2018-05-18 18:39:44 +0800

    Init

---
+++ Palindromes/回文測試/ReverseArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 回文測試
+{
+    public class ReverseArray
+    {
+        public bool Rever(string conso)
+        {
+            char[] text;
+            string revertext;
+            revertext = conso.Replace(" ","");
+            text = revertext.ToCharArray();
+            Array.Reverse(text);
+            return revertext == new string(text);
+        }
+    }
+}
